a4de3b144ab0edaa7d19c61edfd86452f5678b72
Glyn Hudson <glyn.hudson@gmail.com>
2019-11-22 17:06:29 +0000

    bumb verison V1.4.0

--- src/EmonTxV3CM.ino
+++ src/EmonTxV3CM.ino
@@ -32,7 +32,7 @@ copy the following in to emonhub.conf:
 #include <Arduino.h>
 #include <avr/wdt.h>
 
-const byte version = 13;                                 // Firmware version divide by 10 to get version number e,g 05 = v0.5
+const byte version = 14;                                 // Firmware version divide by 10 to get version number e,g 05 = v0.5
 
 // Comment/Uncomment as applicable
 #define ENABLE_RF                                       // Enable RF69 transmit, turn off if using direct serial, or EmonESP
